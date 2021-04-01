78a45b4e92bb8377e395f47be0e6397beea8281e
Karen Gabriella Rodrigues dos Santos <karengabriellarodrigues@gmail.com>
2021-04-01 18:35:57 -0300

    Cadastro - Cliente

---
+++ src/util/Node.java
@@ -0,0 +1,25 @@
+package util;
+
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+
+   
+}
