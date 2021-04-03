f9aec6607b092c11ac0153aa32e86252ee4e6966
thcarvalho <th29.br@gmail.com>
2021-04-03 17:08:04 -0300

    método read do Client funfando

--- src/controllers/ClientController.java
+++ src/controllers/ClientController.java
@@ -5,20 +5,33 @@ import models.Client;
 import services.TXTService;
 
 public class ClientController {
-	private TXTService<Client> txtService;
+    private TXTService<Client> txtService;
 
-	public ClientController(TXTService<Client> txtService) {
-		this.txtService = txtService;
-	}
+    public ClientController(TXTService<Client> txtService) {
+        this.txtService = txtService;
+    }
 
-	public void create(Client client) {
-		// Dá pra colocar a lógica de verificar CPF aqui
-		txtService.create(client);
-	}
+    public void create(Client client) {
+        // Dá pra colocar a lógica de verificar CPF aqui
+        txtService.create(client);
+    }
 
-	public List<Client> read() {
-		// Ainda arrumar isso
-//		List<String> clientTXT = txtService.read();
-		return new List<>();
-	}
+    public List<Client> read() {
+        List<String> clientTXT = txtService.read();
+        List<Client> clients = new List<>();
+
+        String[] lines = clientTXT.toString().split("\n");
+        for (String line : lines) {
+			String[] data = line.split(",");
+			String name = data[0];
+			String CPF = data[1];
+			String email = data[2];
+			String tel = data[3];
+
+			Client client = new Client(CPF, name, email, tel);
+			clients.add(client);
+        }
+
+        return clients;
+    }
 }
