879e84e3e1d88a470ae7c63f958e145176cc905c
thcarvalho <th29.br@gmail.com>
2021-04-03 01:56:03 -0300

    teste de funcionalidade

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import controllers.ClientController;
+import list.List;
+import models.Client;
+import services.TXTService;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		TXTService<Client> txtService = new TXTService<>("client.txt");
+		ClientController clientController = new ClientController(txtService);
+		Client client = new Client("123123123", "Teste", "teste@teste", "1123452345");
+		clientController.create(client);
+		List<String> list = txtService.read();
+		System.out.println(list.toString());
 	}
 }
