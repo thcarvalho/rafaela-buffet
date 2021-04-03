de6066b2c7a7c3e6a95da2af26c38d52afbdc548
thcarvalho <th29.br@gmail.com>
2021-04-03 18:02:24 -0300

    teste de delete

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		TXTService<Client> txtService = new TXTService<>("client.txt");
 		ClientController clientController = new ClientController(txtService);
-		Client client = new Client("123123123", "Teste", "teste@teste", "1123452345");
-		clientController.create(client);
-		List<String> list = txtService.read();
-		System.out.println(list.toString());
+//		Client client = new Client("123123123", "Teste", "teste@teste", "1123452345");
+//		clientController.create(client);
+//		List<String> list = txtService.read();
+//		System.out.println(list.toString());
+		clientController.delete("123123123");
 	}
 }
